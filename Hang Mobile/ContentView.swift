c46f9618586dea7790d71647fd466f967facf27b
fahmi-dzulqarnain <fdz.fhz1302@gmail.com>
2022-01-13 21:58:08 +0700

    Initial Commit

---
+++ Hang Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hang Mobile
+//
+//  Created by Fahmi Dzulqarnain on 13/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hang Mobile/Hang_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hang_MobileApp.swift
+//  Hang Mobile
+//
+//  Created by Fahmi Dzulqarnain on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Hang_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
